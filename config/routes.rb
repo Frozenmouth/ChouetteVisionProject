acfd30367f1958071c4e0f7eef1632d22f695053
Alexis Martin <alexismartin232@gmail.com>
2019-05-15 09:13:12 +0200

    Ajout de derniere minute

--- config/routes.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.routes.draw do
-  get '/', to: 'page#index'
-  get '/condition_use', to: 'page#condition_use'
-  get '/policy_of_confidenciality', to:'page#policy_of_confidenciality'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
